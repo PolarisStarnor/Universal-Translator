5f79b8c395d5c7f41b4b1793cda17fc1844215d0
Ayrton <achilibe@ualberta.ca>
2024-01-06 12:09:18 -0700

    Added stub for transcription file

---
+++ server/speech_to_text.py
@@ -0,0 +1,11 @@
+# Takes an audio file and turns it into text
+
+def transcribe(speech) -> str:
+    """
+    Translate an audio file into a text file.
+
+    @param speech: an audio file
+
+    @return text: the transcription of the audio file
+    """
+    pass
