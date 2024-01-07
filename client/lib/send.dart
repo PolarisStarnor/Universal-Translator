c4c35b2ed669cdea340b723843a067cfd8d7534b
Ayrton <achilibe@ualberta.ca>
2024-01-07 05:15:13 -0700

    Changed the socket to the correct address

--- client/lib/send.dart
+++ client/lib/send.dart
@@ -8,7 +8,7 @@ void send(String path) async {
   // File file2 = File("test_audio/test_write.mp3");
   // file2.writeAsBytes(bytes);
 
-  Socket.connect("localhost", 14000).then((socket) async {
+  Socket.connect("hacked.blobfish.icu", 14000).then((socket) async {
     await socket.addStream(bytes);
     socket.destroy();
   });
