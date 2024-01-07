c6148b45780bb7ad70601e9c58ceeba48a135656
Ayrton <achilibe@ualberta.ca>
2024-01-07 03:24:52 -0700

    Added send example

---
+++ client/lib/send.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+// import 'dart:typed_data';
+
+void main() async {
+  File file = File("test_audio/test.mp3");
+  var bytes = file.openRead();
+
+  // File file2 = File("test_audio/test_write.mp3");
+  // file2.writeAsBytes(bytes);
+
+  Socket.connect("localhost", 14000).then((socket) async {
+    await socket.addStream(bytes);
+    socket.destroy();
+  });
+}
