03db7f4969899ed76296ad15841b7443cc34e2ff
Ayrton <achilibe@ualberta.ca>
2024-01-06 17:26:59 -0700

    Modified Gitignore

--- server/tests/test_specch_to_text.py
+++ server/tests/test_specch_to_text.py
@@ -3,7 +3,7 @@ import pathlib
 import speech_to_text
 import warnings
 
-class TestStringMethods(unittest.TestCase):
+class TestAudioToText(unittest.TestCase):
 
     def test_audio_input(self):
         file: pathlib.Path = pathlib.Path('tests/test_audio/this_is_a_test_recording.mp3')
@@ -12,6 +12,13 @@ class TestStringMethods(unittest.TestCase):
         self.assertIn("this is a test recording", transcription.lower())
         if (transcription.lower() != "this is a test recording"):
             warnings.warn("The sequences are not exactly equal:\n    Transcription raw output: " + transcription)
+
+    def test_long_audio(self):
+        file: pathlib.Path = pathlib.Path('tests/test_audio/convoluded.mp3')
+        transcription: str = speech_to_text.transcribe(file)
+
+        self.assertIn("this is a convoluted example", transcription.lower())
+
 
 if __name__ == '__main__':
     unittest.main()
