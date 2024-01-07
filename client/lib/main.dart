74cd84c20637bcc41afe90a22443cac262d78373
PolarisStarnor <polarisstarnor@gmail.com>
2024-01-07 05:24:34 -0700

    Scuffed as all hell enumeration fix, it works, it's fine, we move on.

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,69 +4,10 @@ import 'package:universal_translator/send.dart';
 
 import 'recorder.dart';
 import 'package:path_provider/path_provider.dart';
+import 'languages.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-enum Languages {
-  english("English", "en"),
-  french("French", "fr"),
-  arabic("Arabic", "ar"),
-  czech("Czech", "cs"),
-  german("German", "de"),
-  spanish("Spanish", "es"),
-  estonian("Estonian", "et"),
-  finnish("Finnish", "fi"),
-  gujarati("Gujarati", "gu"),
-  hindi("Hindi", "hi"),
-  italian("Italian", "it"),
-  japanese("Japanese", "ja"),
-  kazakh("Kazakh", "kk"),
-  korean("Korean", "ko"),
-  lithuanian("Lithuanian", "lt"),
-  latvian("Latvian", "lv"),
-  burmese("Burmese", "my"),
-  nepali("Nepali", "ne"),
-  dutch("Dutch", "nl"),
-  romanian("Romanian", "ro"),
-  russian("Russian", "ru"),
-  sinhala("Sinhala", "si"),
-  turkish("Turkish", "tr"),
-  vietnamese("Vietnamese", "vi"),
-  chinese("Chinese", "zh"),
-  afrikaans("Afrikaans", "af"),
-  azerbaijani("Azerbaijani", "az"),
-  bengali("Bengali", "bn"),
-  persian("Persian", "fa"),
-  hebrew("Hebrew", "he"),
-  croatian("Croatian", "hr"),
-  indonesian("Indonesian", "id"),
-  georgian("Georgian", "ka"),
-  khmer("Khmer", "km"),
-  macedonian("Macedonian", "mk"),
-  malayalam("Malayalam", "ml"),
-  mongolian("Mongolian", "mn"),
-  marathi("Marathi", "mr"),
-  polish("Polish", "pl"),
-  pashto("Pashto", "ps"),
-  portuguese("Portuguese", "pt"),
-  swedish("Swedish", "sv"),
-  swahili("Swahili", "sw"),
-  tamil("Tamil", "ta"),
-  telugu("Telugu", "te"),
-  thai("Thai", "th"),
-  tagalog("Tagalog", "tl"),
-  ukrainian("Ukrainian", "uk"),
-  urdu("Urdu", "ur"),
-  xhosa("Xhosa", "xh"),
-  galician("Galician", "gl"),
-  slovene("Slovene", "sl");
-
-  const Languages(this.label, this.val);
-  final String label;
-  final String val;
-
 }
 
 class MyApp extends StatelessWidget {
@@ -164,8 +105,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    Languages? selectedLang;
-    final TextEditingController languageController = TextEditingController();
+    inputLanguages? inputLang;
+    outputLanguages? outputLang;
+    final TextEditingController inputLanguageController = TextEditingController();
+    final TextEditingController outputLanguageController = TextEditingController();
     Icon recordIcon = const Icon(Icons.fiber_manual_record_outlined);
 
     return Scaffold(
@@ -197,24 +140,24 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            DropdownMenu<Languages>(
-              initialSelection: Languages.english,
-              controller: languageController,
+            DropdownMenu<inputLanguages>(
+              initialSelection: inputLanguages.english,
+              controller: inputLanguageController,
               // requestFocusOnTap is enabled/disabled by platforms when it is null.
               // On mobile platforms, this is false by default. Setting this to true will
               // trigger focus request on the text field and virtual keyboard will appear
               // afterward. On desktop platforms however, this defaults to true.
               requestFocusOnTap: true,
               label: const Text('Input Language'),
-              onSelected: (Languages? lang) {
+              onSelected: (inputLanguages? lang) {
                 setState(() {
-                  selectedLang = lang;
+                  inputLang = lang;
                 });
               },
-              dropdownMenuEntries: Languages.values
-                  .map<DropdownMenuEntry<Languages>>(
-                      (Languages lang) {
-                    return DropdownMenuEntry<Languages>(
+              dropdownMenuEntries: inputLanguages.values
+                  .map<DropdownMenuEntry<inputLanguages>>(
+                      (inputLanguages lang) {
+                    return DropdownMenuEntry<inputLanguages>(
                       label: lang.label,
                       value: lang,
                     );
@@ -223,24 +166,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
             const SizedBox(height: 30),
 
-            DropdownMenu<Languages>(
-              initialSelection: Languages.english,
-              controller: languageController,
+            DropdownMenu<outputLanguages>(
+              initialSelection: outputLanguages.english,
+              controller: outputLanguageController,
               // requestFocusOnTap is enabled/disabled by platforms when it is null.
               // On mobile platforms, this is false by default. Setting this to true will
               // trigger focus request on the text field and virtual keyboard will appear
               // afterward. On desktop platforms however, this defaults to true.
               requestFocusOnTap: true,
               label: const Text('Output Language'),
-              onSelected: (Languages? lang) {
+              onSelected: (outputLanguages? lang) {
                 setState(() {
-                  selectedLang = lang;
+                  outputLang = lang;
                 });
               },
-              dropdownMenuEntries: Languages.values
-                  .map<DropdownMenuEntry<Languages>>(
-                      (Languages lang) {
-                    return DropdownMenuEntry<Languages>(
+              dropdownMenuEntries: outputLanguages.values
+                  .map<DropdownMenuEntry<outputLanguages>>(
+                      (outputLanguages lang) {
+                    return DropdownMenuEntry<outputLanguages>(
                       label: lang.label,
                       value: lang,
                     );
