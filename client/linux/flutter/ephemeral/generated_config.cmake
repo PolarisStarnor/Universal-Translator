0374d8768c515d0ba67951b2d3fd8e873e8e8903
Ayrton <achilibe@ualberta.ca>
2024-01-07 04:40:46 -0700

    Added other platforms for debugging

---
+++ client/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/stormblessed/Code/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/stormblessed/Code/Universal-Translator/client" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/stormblessed/Code/flutter"
+  "PROJECT_DIR=/home/stormblessed/Code/Universal-Translator/client"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8zOWM2ZGM5YmRkM2JhMmJjMzg3ZTZlMTRiNzJmY2U1ZjQ2YWJmY2FlLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/stormblessed/Code/Universal-Translator/client/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/stormblessed/Code/Universal-Translator/client/lib/main.dart"
+)
